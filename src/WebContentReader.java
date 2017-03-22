05b99eb26debc9275259dc807e16ccf99cff059b
Sebastian Hunkeler <hunkeler.sebastian@gmail.com>
2017-03-22 22:00:21 +0100

    Introduced random delay for web content

--- src/WebContentReader.java
+++ src/WebContentReader.java
@@ -1,23 +1,47 @@
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class WebContentReader {
-	private static final int MAX_CONNECTION_DELAY = 5000;
+	private final static int MIN_CONNECTION_DELAY = 3000;
+	private static final int MAX_CONNECTION_DELAY = 6000;
 	private HTTPReader httpReader;
 	private SoundPlayer soundPlayer;
 	private SpeechPlayer speechPlayer;
+	
+	private Timer delayTimer;
+	private TimerTask task;
 	
 	public WebContentReader(SoundPlayer soundPlayer, SpeechPlayer speechPlayer) {
 		this.soundPlayer = soundPlayer;
 		this.speechPlayer = speechPlayer;
 		httpReader = new HTTPReader();
+		delayTimer = new Timer();
 	}
 
 	public void read(String ipAddress){
 		Random randomizer = new Random();
-    	int delay = randomizer.nextBoolean() ? randomizer.nextInt(MAX_CONNECTION_DELAY) : 0;
+		int delay = randomizer.nextBoolean() ? MIN_CONNECTION_DELAY + randomizer.nextInt(MAX_CONNECTION_DELAY - MIN_CONNECTION_DELAY) : 0;
     	
-        String webpageText = httpReader.getWebPageBody(ipAddress);
+    	System.out.println("Loading " + ipAddress + " in " + delay + "ms.");
+    	
+    	if(delay > 0) {
+    		speechPlayer.say("Your page is loading. Please hang up, we'll call you back.");
+    	}
+    	
+    	if(task != null) task.cancel();
+		task = new TimerTask() {
+			@Override
+			public void run() {
+				loadAndReadWebContent(ipAddress);
+			}
+		};
+		delayTimer.schedule( task, delay);
+	}
+
+	private void loadAndReadWebContent(String ipAddress) {
+		String webpageText = httpReader.getWebPageBody(ipAddress);
         
         if (webpageText != null) {
         	soundPlayer.stop();
