e6825ff09f36f3e47b41c72b9f961416cdbb09dc
Sebastian Hunkeler <hunkeler.sebastian@gmail.com>
2017-03-22 23:55:32 +0100

    Implemented Mode Switching

--- src/StateManager.java
+++ src/StateManager.java
@@ -1,6 +1,35 @@
+enum Mode {
+	Incognito,
+	History,
+	Article,
+	Developer
+}
 
 public class StateManager {
 	public boolean reiceverPickedUp = false;
 	public boolean callingPhone = false;
 	public int lastHangupTime = 0;
+	public Mode currentMode = Mode.Article;
+	
+	public void setMode(String modeCommand){
+		String modeSymbol = modeCommand.substring(2);
+		System.out.println("Setting mode: " + modeSymbol);
+		
+		switch (modeSymbol) {
+		case "i":
+			currentMode = Mode.Incognito;
+			break;
+		case "h":
+			currentMode = Mode.History;
+			break;
+		case "a":
+			currentMode = Mode.Article;
+			break;
+		case "d":
+			currentMode = Mode.Developer;
+			break;
+		default:
+			break;
+		}
+	}
 }
